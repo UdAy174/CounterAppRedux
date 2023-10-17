39d8a5aa339722769d8486240c9fed150e99ccd3
uday mahto <uday28665@gmail.com>
2023-10-17 20:36:39 +0530

    slicer created

--- src/redux/slices/CounterSlice.jsx
+++ src/redux/slices/CounterSlice.jsx
@@ -8,6 +8,14 @@ const CounterSlice = createSlice({
     name:"counter",
     initialState,
     reducers:{
-        
+        increment : (state) => {
+            state.value +=1;
+        },
+        decrement : (state)=> {
+            state.value -=1;
+        }
     }
 })
+
+export const {increment,decrement} = CounterSlice.actions;
+export default CounterSlice.reducer;
