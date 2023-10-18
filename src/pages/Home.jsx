5ca38f6a92b1bdebba1046ea8596fbd802cf1414
uday mahto <uday28665@gmail.com>
2023-10-18 15:06:57 +0530

    first commit of cart app

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+    const API_URL = "https://fakestoreapi.com/products";
+  
+    return <div>Home</div>;
+};
+  
+export default Home;
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import CounterSlice from "./slices/CounterSlice";
+import Cart from "./slices/Cart";
 
 export const store = configureStore({
     reducer : {
-        counter : CounterSlice
+        counter : 
     },
 })
